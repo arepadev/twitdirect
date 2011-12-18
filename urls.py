ba6a8018b1b01e9e2da778340665ecf8104a9b61
Carlos Guerrero <guerrerocarlos@gmail.com>
2011-12-18 01:53:46 -0430

    Adding basic view

--- urls.py
+++ urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'adopta.views.home', name='home'),
+    url(r'^$', 'adoptave.webui.views.home', name='home'),
     # url(r'^adopta/', include('adopta.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -14,4 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    (r'^(?P<path>.*)$', 'django.views.static.serve',{'document_root': '/home/guerrerocarlos/adoptave/templates/'}),
 )
